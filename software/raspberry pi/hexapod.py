7bf1646d1e7028ae02cf2a86937a71e6ec1d3b62
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2022-02-16 21:04:52 -0500

    Update hexapod.py

--- software/raspberry pi/hexapod.py
+++ software/raspberry pi/hexapod.py
@@ -147,7 +147,7 @@ class Hexapod(Thread):
 
         # self.standby()
         # self.laydown()
-        self.leg_0.move_junctions([90,90,90])
+        self.leg_0.move_junctions([90,90,45])
         time.sleep(1)
 
         # self.leg_0.set_angle(1, 30)
