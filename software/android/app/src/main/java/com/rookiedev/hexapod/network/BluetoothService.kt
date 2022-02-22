7136a33f63a5e1806a8314584ff2995bb8331d5f
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2022-02-22 12:07:52 -0500

    update UI

--- software/android/app/src/main/java/com/rookiedev/hexapod/network/BluetoothService.kt
+++ software/android/app/src/main/java/com/rookiedev/hexapod/network/BluetoothService.kt
@@ -398,20 +398,6 @@ class BluetoothService(context: Context?, handler: Handler) {
             // given BluetoothDevice
             try {
                 tmp = if (secure) {
-//                    if (ActivityCompat.checkSelfPermission(
-//                            this,
-//                            Manifest.permission.BLUETOOTH_CONNECT
-//                        ) != PackageManager.PERMISSION_GRANTED
-//                    ) {
-//                        // TODO: Consider calling
-//                        //    ActivityCompat#requestPermissions
-//                        // here to request the missing permissions, and then overriding
-//                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//                        //                                          int[] grantResults)
-//                        // to handle the case where the user grants the permission. See the documentation
-//                        // for ActivityCompat#requestPermissions for more details.
-//                        return
-//                    }
                     mmDevice.createRfcommSocketToServiceRecord(
                         MY_UUID_SECURE
                     )
