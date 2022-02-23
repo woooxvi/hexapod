0e142f4e66c0c97d5ab6632f76b5437e241be070
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2022-02-22 23:32:13 -0500

    Update MainActivity.kt

--- software/android/app/src/main/java/com/rookiedev/hexapod/MainActivity.kt
+++ software/android/app/src/main/java/com/rookiedev/hexapod/MainActivity.kt
@@ -14,7 +14,6 @@ import android.text.method.LinkMovementMethod
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
