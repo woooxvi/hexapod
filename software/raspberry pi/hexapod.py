d27a9d81a32a3cd3f8501f498811c6142824bcb4
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2022-02-27 15:52:47 -0500

    Update hexapod.py

--- software/raspberry pi/hexapod.py
+++ software/raspberry pi/hexapod.py
@@ -115,33 +115,33 @@ class Hexapod(Thread):
 
         # front right
         self.leg_0 = Leg(0,
-                         [self.pca_left.servo[15], self.pca_left.servo[2],
-                             self.pca_left.servo[1]],
+                         [self.pca_left.servo[15], self.pca_left.servo[14],
+                             self.pca_left.servo[13]],
                          correction=[4, 6, 2])
         # center right
         self.leg_1 = Leg(1,
-                         [self.pca_left.servo[7], self.pca_left.servo[8],
-                             self.pca_left.servo[6]],
+                         [self.pca_left.servo[8], self.pca_left.servo[4],
+                             self.pca_left.servo[11]],
                          correction=[0, 8, -6])
         # rear right
         self.leg_2 = Leg(2,
-                         [self.pca_left.servo[0], self.pca_left.servo[14],
-                             self.pca_left.servo[13]],
+                         [self.pca_left.servo[0], self.pca_left.servo[1],
+                             self.pca_left.servo[2]],
                          correction=[2, 8, -1])
         # rear left
         self.leg_3 = Leg(3,
-                         [self.pca_right.servo[15], self.pca_right.servo[1],
-                             self.pca_right.servo[2]],
+                         [self.pca_right.servo[15], self.pca_right.servo[14],
+                             self.pca_right.servo[13]],
                          correction=[-3, 10, -8])
         # center left
-        self.leg_4 = Leg(4,
-                         [self.pca_right.servo[7], self.pca_right.servo[6],
-                             self.pca_right.servo[8]],
+        self.leg_4 = Leg(4, 
+                         [self.pca_right.servo[7], self.pca_right.servo[11],
+                             self.pca_right.servo[4]],
                          correction=[-6, 2, -4])
         # front left
         self.leg_5 = Leg(5,
-                         [self.pca_right.servo[0], self.pca_right.servo[13],
-                             self.pca_right.servo[14]],
+                         [self.pca_right.servo[0], self.pca_right.servo[1],
+                             self.pca_right.servo[2]],
                          correction=[0, 0, -10])
 
         # self.leg_0.reset(True)
